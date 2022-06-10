af41c319fbf270f2c44248ba0300d8747e6623dd
A.Stadler <adamv.stadler@gmail.com>
2022-06-10 00:17:15 -0400

    feat: optimism, colors

--- src/Spell.svelte
+++ src/Spell.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import { convert } from "./utils/numerals";
     import { alpha2school } from "./srd/school";
-    export let spell;
+    export let spell: any;
+    export let click: () => void = () => {};
 </script>
 
 <li class="spell p-0">
-    <button class={alpha2school(spell.school)}
+    <button class={alpha2school(spell.school)} on:click={click}
         >{spell.name}
         {spell.level !== 0 ? convert(spell.level) : ""}</button
     >
--- src/Spellbook.svelte
+++ src/Spellbook.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-    import { classes, randomClass } from "./srd/classes";
+    import { onMount } from "svelte";
+    import type { prep } from "./prep";
+    import Spell from "./Spell.svelte";
+    import { classes } from "./srd/classes";
     import { spells } from "./srd/spells-phb";
     import { getLazy } from "./utils/lazy";
-    import { convert } from "./utils/numerals";
-    import { CharacterStorage } from "./utils/storage";
-    import type { prep } from "./prep";
-    import { onMount } from "svelte";
-    import Spell from "./Spell.svelte";
-    let classSpells = [];
+    import { getOptimism } from "./srd/optimism";
+
+    let allClassSpells = [];
     let maxSlotLevel = 0;
     let toPrepare = 0;
     export let prep: prep;
 
     const mod = (score: number) => Math.floor((score - 10) / 2);
     const refreshSpells = () =>
-        (classSpells = spells
+        (allClassSpells = spells
             .filter(
                 (spell) =>
                     spell.classes.fromClassList.find(
@@ -28,14 +28,14 @@
         maxSlotLevel = prep.clazz.classTableGroups
             .find((group) => group.rowsSpellProgression != null)
             ?.rowsSpellProgression[prep.level - 1].findIndex((n) => n === 0);
-        toPrepare = calculatePrepared();
+        calculatePrepared();
     };
     const calculatePrepared = () => {
         let caster = 1;
         if (prep.clazz.casterProgression === "1/2") {
             caster = 0.5;
         }
-        return Math.floor(
+        toPrepare = Math.floor(
             (mod(prep.score) + prep.level) * caster - prep.prepared.length
         );
     };
@@ -80,55 +80,84 @@
         <button>save</button>
     </p>
     <hr />
-    <div class="grid grid-cols-3">
-        <div>
-            <h2>
-                Choose {prep.clazz.cantripProgression != null
-                    ? prep.clazz.cantripProgression[prep.level + 1]
-                    : 0} cantrips:
-            </h2>
-            <hr />
-            <ul>
-                {#each classSpells.filter((s) => s.level === 0) as cantrip}
-                    <Spell
-                        spell={cantrip}
-                        on:click={() =>
-                            (prep.catnips = prep.catnips.concat(cantrip))}
-                    />
-                {/each}
-            </ul>
+    <div class="grid grid-rows-2">
+        <div class="grid grid-cols-3">
+            <div>
+                <h2>
+                    Choose {prep.clazz.cantripProgression != null
+                        ? prep.clazz.cantripProgression[prep.level + 1] -
+                          prep.catnips.length
+                        : 0} cantrips:
+                </h2>
+                <hr />
+                <ul>
+                    {#each allClassSpells.filter((s) => s.level === 0 && !prep.catnips.find((c) => s.name === c.name)) as catnip}
+                        <Spell
+                            spell={catnip}
+                            click={() =>
+                                (prep.catnips = prep.catnips.concat(catnip))}
+                        />
+                    {/each}
+                </ul>
+            </div>
+            <div>>></div>
+            <div>
+                <h2>Chosen cantrips:</h2>
+                <hr />
+                <ul>
+                    {#each prep.catnips as catnip}
+                        <Spell
+                            spell={catnip}
+                            click={() =>
+                                (prep.catnips = prep.catnips.filter(
+                                    (c) => c.name !== catnip.name
+                                ))}
+                        />
+                    {/each}
+                </ul>
+            </div>
         </div>
-        <div>>></div>
-        <div>
-            <h2>Chosen cantrips:</h2>
-            <hr />
-            <ul>
-                {#each prep.catnips as catnip}
-                    <Spell
-                        spell={catnip}
-                        on:click={() =>
-                            (prep.catnips = prep.catnips.filter(
-                                (c) => c.name !== catnip.name
-                            ))}
-                    />
-                {/each}
-            </ul>
-        </div>
-        <div>
-            <h2>
-                You have {toPrepare} spell{toPrepare === 1 ? "" : "s"} to prepare.
-            </h2>
-            <hr />
-            <ul class="overflow-auto">
-                {#each classSpells.filter((s) => s.level !== 0 && s.level <= maxSlotLevel) as spell}
-                    <Spell {spell} />
-                {/each}
-            </ul>
-        </div>
-        <div><span>>></span></div>
-        <div>
-            prepared spelluloids
-            <hr />
+        <div class="grid grid-cols-3">
+            <div>
+                <h2>
+                    You have {toPrepare} spell{toPrepare === 1 ? "" : "s"}
+                    to prepare.
+                </h2>
+                <hr />
+                <ul class="overflow-auto">
+                    {#if toPrepare > 0}
+                        {#each allClassSpells.filter((s) => !prep.prepared.find((c) => s.name === c.name) && s.level !== 0 && s.level <= maxSlotLevel) as spell}
+                            <Spell
+                                {spell}
+                                click={() => {
+                                    prep.prepared = prep.prepared.concat(spell);
+                                    calculatePrepared();
+                                }}
+                            />
+                        {/each}
+                    {:else}
+                        {getOptimism()}
+                    {/if}
+                </ul>
+            </div>
+            <div><span>>></span></div>
+            <div>
+                <h2>prepared spelluloids</h2>
+                <hr />
+                <ul class="overflow-auto">
+                    {#each prep.prepared as spell}
+                        <Spell
+                            {spell}
+                            click={() => {
+                                prep.prepared = prep.prepared.filter(
+                                    (p) => p.name !== spell.name
+                                );
+                                calculatePrepared();
+                            }}
+                        />
+                    {/each}
+                </ul>
+            </div>
         </div>
     </div>
 </div>
