f430106bc92f7592f352e988eac70cc4e7f894ea
A. Stadler <adamv.stadler@gmail.com>
2023-07-30 23:03:11 -0400

    feat: concentration icon and text (#5)
    * feat: concentration icon and text
    
    * feat: added ritual icon & text.

--- src/Spell.svelte
+++ src/Spell.svelte
@@ -1,29 +1,35 @@
 <script lang="ts">
-    import { convert } from "./utils/numerals";
-    import { alpha2school } from "./srd/school";
-    import type { SRDSpell } from "./srd/SRDSpell";
-    import SpellDetails from "./SpellDetails.svelte";
-    import { createEventDispatcher } from "svelte";
-    export let spell: any;
-    export let click: () => void = () => {};
-    let showInfo = false;
-    const emitter = createEventDispatcher();
-    const info = () => emitter("message", spell);
+  import { convert } from "./utils/numerals";
+  import { alpha2school } from "./srd/school";
+  import type { SRDSpell } from "./srd/SRDSpell";
+  import SpellDetails from "./SpellDetails.svelte";
+  import { createEventDispatcher } from "svelte";
+  export let spell: any;
+  export let click: () => void = () => {};
+  let showInfo = false;
+  const emitter = createEventDispatcher();
+  const info = () => emitter("message", spell);
 </script>
 
 <li class="spell p-0">
-    <button class={alpha2school(spell.school)} on:click={click}
-        >{spell.name}
-        {spell.level !== 0 ? convert(spell.level) : ""}</button
-    ><button on:click={info}><i class="las la-search" /></button>
-    <p class="overflow-auto max-h-48 max-w-48" hidden={!showInfo}>
-        <SpellDetails {spell} />
-    </p>
+  <button class={alpha2school(spell.school)} on:click={click}>
+    {#if spell.meta?.ritual}
+      <i class="las la-share-alt" />
+    {/if}
+    {#if spell.duration[0].concentration}
+      <i class="las la-eye" />
+    {/if}
+    {spell.name}
+    {spell.level !== 0 ? convert(spell.level) : ""}
+  </button><button on:click={info}><i class="las la-search" /></button>
+  <p class="overflow-auto max-h-48 max-w-48" hidden={!showInfo}>
+    <SpellDetails {spell} />
+  </p>
 </li>
 
 <style>
-    button {
-        margin: 1px;
-        border-radius: 5px;
-    }
+  button {
+    margin: 1px;
+    border-radius: 5px;
+  }
 </style>
